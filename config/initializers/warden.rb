8895cc82c55fac2540dde588d1ad98fd35ee32d1
dblock <dblock@dblock.org>
2018-07-07 22:21:00 -0400

    Serialize user ID, not the entire session.

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,9 +1,7 @@
 Rails.application.config.middleware.insert_after Rack::ETag, Warden::Manager do |manager|
   manager.failure_app = GraphqlController
 
-  Warden::Manager.serialize_into_session do |user|
-    user.id
-  end
+  Warden::Manager.serialize_into_session(&:id)
 
   Warden::Manager.serialize_from_session do |id|
     User.find(id)
