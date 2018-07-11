74fd86755af814745fe1e2aeb4b9c427c8d6e0cb
dblock <dblock@dblock.org>
2018-07-10 21:31:39 -0400

    Return null user when not logged in.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,7 +5,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   field :user, !Types::UserType do
     description 'Get current user.'
     resolve ->(_obj, _args, ctx) {
-      ctx[:current_user] || GraphQL::ExecutionError.new('Not logged in.')
+      ctx[:current_user] || { id: nil }
     }
   end
 end
--- spec/graphql/queries/user_spec.rb
+++ spec/graphql/queries/user_spec.rb
@@ -1,22 +1,44 @@
 require 'rails_helper'
 
 describe 'Current User Query', type: :request do
-  include_context 'Authenticated Client'
+  context 'unauthenticated' do
+    include_context 'GraphQL Client'
 
-  let(:query) do
-    <<-GRAPHQL
-      query {
-        user {
-          id
-          email
+    let(:query) do
+      <<-GRAPHQL
+        query {
+          user {
+            id
+          }
         }
-      }
-    GRAPHQL
+      GRAPHQL
+    end
+
+    it 'returns null id' do
+      response = client.execute(query)
+      user = response.data.user
+      expect(user.id).to be nil
+    end
   end
 
-  it 'returns an current_user' do
-    response = client.execute(query)
-    user = response.data.user
-    expect(user.id).to eq current_user.id.to_s
+  context 'authenticated' do
+    include_context 'Authenticated Client'
+
+    let(:query) do
+      <<-GRAPHQL
+        query {
+          user {
+            id
+            email
+          }
+        }
+      GRAPHQL
+    end
+
+    it 'returns the current_user' do
+      response = client.execute(query)
+      user = response.data.user
+      expect(user.id).to eq current_user.id.to_s
+    end
   end
 end
